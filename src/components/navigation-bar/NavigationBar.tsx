54a41998734ddfaa19635b86e2bba9e4d6d923a9
washington-shoji <shoji.washington@gmail.com>
2022-02-06 19:56:08 +1100

    refactor(component): Refactored the navigation bar component and styling

--- src/components/navigation-bar/NavigationBar.tsx
+++ src/components/navigation-bar/NavigationBar.tsx
@@ -2,31 +2,20 @@ import React from 'react';
 import './NavigationBar.scss';
 import logo from '../../assets/images/evx-logo-1.png';
 import NavigationBarMenu, { ILinksProps } from './NavigationBarMenu';
-
-const links: ILinksProps[] = [
-	{
-		label: 'home',
-		link: '/',
-	},
-	{
-		label: 'events',
-		link: '/events',
-	},
-	{
-		label: 'profile',
-		link: '/profile',
-	},
-];
+import { Link } from 'react-router-dom';
+import { links } from '../../data/dummy-data/dummy-links';
 
 export default function NavigationBar() {
 	return (
 		<div className='navigation-bar'>
 			<div className='navigation-bar__logo'>
-				<img src={logo} alt='' />
+				<Link to='/'>
+					<img src={logo} alt='' />
+				</Link>
 			</div>
 			<div className='navigation-bar__menu__container'>
-				{links.map((link: ILinksProps) => {
-					return <NavigationBarMenu {...link} />;
+				{links.map((link: ILinksProps, index: number) => {
+					return <NavigationBarMenu key={index} {...link} />;
 				})}
 			</div>
 		</div>
