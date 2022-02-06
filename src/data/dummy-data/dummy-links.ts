dc98b42f998372c07de0323aa40cd4559a572d58
washington-shoji <shoji.washington@gmail.com>
2022-02-06 19:58:12 +1100

    chore(relocate): Relocated files

---
+++ src/data/dummy-data/dummy-links.ts
@@ -0,0 +1,16 @@
+import { ILinksProps } from '../../components/navigation-bar/NavigationBarMenu';
+
+export const links: ILinksProps[] = [
+	{
+		label: 'home',
+		link: '/',
+	},
+	{
+		label: 'events',
+		link: '/events',
+	},
+	{
+		label: 'profile',
+		link: '/profile',
+	},
+];
