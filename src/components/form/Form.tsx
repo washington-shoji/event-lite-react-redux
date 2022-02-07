957f52c85b32ff0e92899ccecf63e1157ac1db84
washington-shoji <shoji.washington@gmail.com>
2022-02-07 16:22:52 +1100

    refactor(component): Refacored the form component to take image upload

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,35 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Form.scss';
-import { Formik, Field, Form, FormikHelpers } from 'formik';
+import {
+	Formik,
+	Field,
+	Form,
+	FormikHelpers,
+	FormikProps,
+	useField,
+} from 'formik';
 import { IMainCardProps } from '../main/MainCard';
 import { useLocation } from 'react-router-dom';
 import Button from '../button/Button';
+import { cloudinaryImageHandlerHelper } from '../../utils/helpers/cloudinary.helper';
 
 export default function EventForm() {
-	console.log('render');
-
 	const { state } = useLocation<IMainCardProps>();
 
-	const {
-		id,
-		title,
-		date,
-		location,
-		status,
-		shortDescription,
-		fullDescription,
-		image,
-	} = state;
-
+	const [imagePreview, setImagePreview] = useState(state.image);
 	const initialState = {
-		title: title,
-		date: date,
-		location: location,
-		shortDescription: shortDescription,
-		fullDescription: fullDescription,
-		image: image,
-		status: status,
+		title: state.title,
+		date: state.date,
+		location: state.location,
+		shortDescription: state.shortDescription,
+		fullDescription: state.fullDescription,
+		image: imagePreview,
+		status: state.status,
 	};
+
 	return (
 		<div className='form'>
 			<div className='form__header'>
@@ -39,105 +36,125 @@ export default function EventForm() {
 			<div>
 				<Formik
 					initialValues={initialState}
-					onSubmit={(
-						values: IMainCardProps,
-						{ setSubmitting }: FormikHelpers<IMainCardProps>
-					) => {
-						setTimeout(() => {
-							alert(JSON.stringify(values, null, 2));
-							setSubmitting(false);
-						}, 500);
+					onSubmit={async (values: IMainCardProps) => {
+						const parsedImageUrl = await cloudinaryImageHandlerHelper(
+							values.image
+						);
+
+						const data = {
+							title: values.title,
+							date: values.date,
+							location: values.location,
+							shortDescription: values.shortDescription,
+							fullDescription: values.fullDescription,
+							image: parsedImageUrl,
+							status: values.status,
+						};
 					}}
 				>
-					<Form className='form__content'>
-						<div className='form__content__left'>
-							<div className='form__image__inputs'>
-								<label htmlFor='image'>Image</label>
-								<Field
-									id='image'
-									name='image'
-									placeholder='Event image'
-									className='form__field'
-								/>
-							</div>
-						</div>
-						<div className='form__content__right'>
-							<div className='form__inputs_2'>
-								<div>
-									<label htmlFor='title'>Title</label>
-									<Field
-										id='title'
-										name='title'
-										placeholder='Event title'
-										className='form__field'
-									/>
-								</div>
-								<div>
-									<label htmlFor='date'>Date</label>
-									<Field
-										id='date'
-										name='date'
-										placeholder='Event date'
-										type='date'
-										className='form__field'
+					{(formProps) => (
+						<Form className='form__content'>
+							<div className='form__content__left'>
+								<img src={imagePreview} alt='' />
+								<div className='form__image__inputs'>
+									<label htmlFor='image'>Image</label>
+									<input
+										id='image'
+										name='image'
+										type='file'
+										onChange={(event: any) => {
+											formProps.setFieldValue(
+												'image',
+												event.currentTarget.files[0]
+											);
+											setImagePreview(
+												URL.createObjectURL(event.target.files[0])
+											);
+										}}
 									/>
 								</div>
 							</div>
+							<div className='form__content__right'>
+								<div className='form__inputs_2'>
+									<div>
+										<label htmlFor='title'>Title</label>
+										<Field
+											id='title'
+											name='title'
+											placeholder='Event title'
+											className='form__field'
+										/>
+									</div>
+									<div>
+										<label htmlFor='date'>Date</label>
+										<Field
+											id='date'
+											name='date'
+											placeholder='Event date'
+											type='date'
+											className='form__field'
+										/>
+									</div>
+								</div>
 
-							<div className='form__inputs_2'>
-								<div>
-									<label htmlFor='location'>Location</label>
+								<div className='form__inputs_2'>
+									<div>
+										<label htmlFor='location'>Location</label>
+										<Field
+											id='location'
+											name='location'
+											placeholder='Event location'
+											className='form__field'
+										/>
+									</div>
+									<div>
+										<label htmlFor='status'>Status</label>
+										<Field
+											id='status'
+											name='status'
+											placeholder='Event status'
+											as='select'
+											className='form__field'
+										>
+											<option value='open'>Open</option>
+											<option value='posted'>Posted</option>
+										</Field>
+									</div>
+								</div>
+
+								<div className='form__inputs'>
+									<label htmlFor='shortDescription'>Short Description</label>
 									<Field
-										id='location'
-										name='location'
-										placeholder='Event location'
+										id='shortDescription'
+										name='shortDescription'
+										placeholder='Event short description'
+										as='textarea'
+										rows='2'
 										className='form__field'
 									/>
 								</div>
-								<div>
-									<label htmlFor='status'>Status</label>
+
+								<div className='form__inputs'>
+									<label htmlFor='fullDescription'>Full description</label>
 									<Field
-										id='status'
-										name='status'
-										placeholder='Event status'
-										as='select'
+										id='fullDescription'
+										name='fullDescription'
+										placeholder='Event full description'
+										as='textarea'
+										rows='10'
 										className='form__field'
-									>
-										<option value='open'>Open</option>
-										<option value='posted'>Posted</option>
-									</Field>
+									/>
+								</div>
+
+								<div>
+									<Button title='Submit' />
 								</div>
 							</div>
-
-							<div className='form__inputs'>
-								<label htmlFor='shortDescription'>Short Description</label>
-								<Field
-									id='shortDescription'
-									name='shortDescription'
-									placeholder='Event short description'
-									as='textarea'
-									rows='2'
-									className='form__field'
-								/>
-							</div>
-
-							<div className='form__inputs'>
-								<label htmlFor='fullDescription'>Full description</label>
-								<Field
-									id='fullDescription'
-									name='fullDescription'
-									placeholder='Event full description'
-									as='textarea'
-									rows='10'
-									className='form__field'
-								/>
-							</div>
-
-							<div>
+							{/* <div>
 								<Button title='Submit' />
-							</div>
-						</div>
-					</Form>
+							</div> */}
+						</Form>
+					)}
 				</Formik>
 			</div>
 		</div>
