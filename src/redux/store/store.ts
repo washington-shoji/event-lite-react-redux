e0e47d1a3485b1e609244e0dac32feb2176012d8
washington-shoji <shoji.washington@gmail.com>
2022-02-09 22:44:24 +1100

    feat(redux): Added redux-toolkit store

---
+++ src/redux/store/store.ts
@@ -0,0 +1,24 @@
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import getEventsReducer from '../slices/get-events-slice';
+import createEventReducer from '../slices/create-event-slice';
+import updateEventReducer from '../slices/update-event-slice';
+
+export const store = configureStore({
+	reducer: {
+		getEvent: getEventsReducer,
+		createEvent: createEventReducer,
+		updateEvent: updateEventReducer,
+	},
+	devTools: true,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+	ReturnType,
+	RootState,
+	unknown,
+	Action<string>
+>;
