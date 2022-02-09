e2f04a42986e8e1ddcc53f79d247825f841b4f85
washington-shoji <shoji.washington@gmail.com>
2022-02-09 22:55:25 +1100

    chore(relocate): Relocated event interface

---
+++ src/interfaces/event.interface.ts
@@ -0,0 +1,10 @@
+export interface IEvent {
+	_id?: string;
+	title: string;
+	date: string;
+	location: string;
+	shortDescription: string;
+	fullDescription: string;
+	image: string;
+	status: string;
+}
