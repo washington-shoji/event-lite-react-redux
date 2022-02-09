4327868b601f427d55895986a847fbc8906ed4d4
washington-shoji <shoji.washington@gmail.com>
2022-02-09 22:46:29 +1100

    feat(redux): Added create event slice

---
+++ src/redux/slices/create-event-slice.ts
@@ -0,0 +1,74 @@
+import { createSlice, Dispatch } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { IEvent } from '../../interfaces/event.interface';
+import { RootState } from '../store/store';
+
+interface IInitialState {
+	createLoading: boolean;
+	createHasError: boolean;
+	createErrorMessage: string[];
+	createdEvent: IEvent;
+}
+
+export const initialState: IInitialState = {
+	createLoading: false,
+	createHasError: false,
+	createErrorMessage: [],
+	createdEvent: {
+		_id: '',
+		title: '',
+		date: '',
+		location: '',
+		shortDescription: '',
+		fullDescription: '',
+		image: '',
+		status: '',
+	},
+};
+
+const createEventOnApiSlice = createSlice({
+	name: 'createEventOnApi',
+	initialState: initialState,
+	reducers: {
+		createEventCall: (state) => {
+			state.createLoading = true;
+		},
+		createEventsSuccess: (state, { payload }) => {
+			state.createLoading = false;
+			state.createHasError = false;
+			state.createdEvent = payload;
+		},
+
+		createEventsFailure: (state, { payload }) => {
+			state.createLoading = false;
+			state.createHasError = true;
+			state.createErrorMessage = payload;
+		},
+	},
+});
+
+export const { createEventCall, createEventsSuccess, createEventsFailure } =
+	createEventOnApiSlice.actions;
+
+export const createEventSelector = (state: RootState) => state.createEvent;
+
+export default createEventOnApiSlice.reducer;
+
+export function createEventOnApiThunk(formData: IEvent) {
+	return async (dispatch: Dispatch): Promise<void> => {
+		dispatch(createEventCall());
+
+		try {
+			const apiResponse = await axios.post<IEvent>(
+				`${process.env.REACT_APP_BASE_API_URL}/event`,
+				formData
+			);
+
+			const data: IEvent = apiResponse.data;
+
+			dispatch(createEventsSuccess(data));
+		} catch (error: any) {
+			dispatch(createEventsFailure(error.message));
+		}
+	};
+}
