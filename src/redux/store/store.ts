2a2dc016bf01ea6065c3b5ef2f9084b800e0d1b7
washington-shoji <shoji.washington@gmail.com>
2022-02-13 20:10:55 +1100

    refactor(minor): Minor refactoring

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,26 +1,26 @@
-import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
-import getEventsReducer from '../slices/get-events-slice';
-import createEventReducer from '../slices/create-event-slice';
-import updateEventReducer from '../slices/update-event-slice';
-import deleteEventReducer from '../slices/delete-event-slice';
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit'
+import getEventsReducer from '../slices/get-events-slice'
+import createEventReducer from '../slices/create-event-slice'
+import updateEventReducer from '../slices/update-event-slice'
+import deleteEventReducer from '../slices/delete-event-slice'
 
 export const store = configureStore({
-	reducer: {
-		getEvent: getEventsReducer,
-		createEvent: createEventReducer,
-		updateEvent: updateEventReducer,
-		deleteEvent: deleteEventReducer,
-	},
-	devTools: true,
-});
+    reducer: {
+        getEvent: getEventsReducer,
+        createEvent: createEventReducer,
+        updateEvent: updateEventReducer,
+        deleteEvent: deleteEventReducer,
+    },
+    devTools: true,
+})
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>
 
-export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch
 
 export type AppThunk<ReturnType = void> = ThunkAction<
-	ReturnType,
-	RootState,
-	unknown,
-	Action<string>
->;
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
+>
